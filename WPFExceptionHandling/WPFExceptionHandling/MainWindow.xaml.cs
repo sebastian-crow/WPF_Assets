83ceb9ee265796c1a99614acc5e9cc1ddf3f73e3
rossezkiller <sberrio@pgsolutionsmiami.com>
2022-08-09 09:07:18 -0500

    First Commit

---
+++ WPFExceptionHandling/WPFExceptionHandling/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace WPFExceptionHandling
+{
+
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ReadFile(0);
+        }
+
+        void ReadFile(int index)
+        {
+            string path = @"C:\Users\SebastianBerrio\source\Text.txt";
+            StreamReader file = new StreamReader(path);
+            char[] buffer = new char[80];
+
+            try
+            {
+                file.ReadBlock(buffer, index, buffer.Length);
+                string str = new string(buffer);
+                throw new Exception();
+                str.Trim();
+                textBox.Text = str;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error reading from " + path + "\nMessage = " + e.Message);
+            }
+            finally
+            {
+                if (file != null)
+                {
+                    file.Close();
+                }
+            }
+        }
+    }
+}
